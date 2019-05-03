a483fa2e4331834cd117e9a654e109147a22fdba
Luis Eduardo Rojas <lerl221295@gmail.com>
2019-05-03 12:33:27 -0300

    updating current time to play button

--- src/components/playResumeButton.js
+++ src/components/playResumeButton.js
@@ -8,11 +8,12 @@ class PlayResumeButton extends ClickableComponent {
     super(player, options);
 
     this.parent = options.parent;
+    player.one('pause', this.update.bind(this));
   }
 
   createEl() {
     const el = dom.createEl('div', {
-      className: `vjs-control vjs-play-resume-button`
+      className: 'vjs-control vjs-play-resume-button'
     });
 
     this.button = el;
@@ -25,23 +26,30 @@ class PlayResumeButton extends ClickableComponent {
 
     const currentTime = videojs.formatTime(this.player().currentTime());
 
-    this.text.innerHTML =  `${this.localize('resumeContent')} ${currentTime}`
+    this.text.innerHTML = `${this.localize('resumeContent')} ${currentTime}`;
 
     el.appendChild(this.span);
     el.appendChild(this.text);
     return el;
   }
 
+  update() {
+    const currentTime = videojs.formatTime(this.player().currentTime());
+
+    this.text.innerHTML = `${this.localize('resumeContent')} ${currentTime}`;
+  }
+
   handleClick(event) {
     this.parent.closed = true;
     this.parent.hide();
-    this.player().play()
+    this.player().play();
   }
 
   resize() {
 
-    if(!this.player_ && !this.player_.el_)
+    if (!this.player_ && !this.player_.el_) {
       return;
+    }
 
     const width = this.player_.el_.offsetWidth;
 
@@ -56,55 +64,55 @@ class PlayResumeButton extends ClickableComponent {
       this.span.style.fontSize = '10px';
       this.span.style.marginLeft = '10px';
 
-      this.button.style.height = "24px";
+      this.button.style.height = '24px';
     } else if (width < 300) {
       this.text.style.fontSize = '12px';
       this.span.style.fontSize = '12px';
       this.span.style.marginLeft = '12px';
 
-      this.button.style.height = "24px";
-      this.button.style.width = "70%";
-      this.button.style.left = "15%";
+      this.button.style.height = '24px';
+      this.button.style.width = '70%';
+      this.button.style.left = '15%';
     } else if (width < 400) {
       this.text.style.fontSize = '16px';
       this.span.style.fontSize = '16px';
       this.span.style.marginLeft = '16px';
 
-      this.button.style.height = "32px";
-      this.button.style.width = "70%";
-      this.button.style.left = "15%";
+      this.button.style.height = '32px';
+      this.button.style.width = '70%';
+      this.button.style.left = '15%';
     } else if (width < 500) {
       this.text.style.fontSize = '18px';
       this.span.style.fontSize = '18px';
       this.span.style.marginLeft = '18px';
 
-      this.button.style.height = "32px";
-      this.button.style.width = "90%";
-      this.button.style.left = "10%";
+      this.button.style.height = '32px';
+      this.button.style.width = '90%';
+      this.button.style.left = '10%';
     } else if (width < 700) {
       this.text.style.fontSize = '22px';
       this.span.style.fontSize = '22px';
       this.span.style.marginLeft = '22px';
 
-      this.button.style.height = "32px";
-      this.button.style.width = "90%";
-      this.button.style.left = "10%";
+      this.button.style.height = '32px';
+      this.button.style.width = '90%';
+      this.button.style.left = '10%';
     } else if (width < 1000) {
       this.text.style.fontSize = '22px';
       this.span.style.fontSize = '22px';
       this.span.style.marginLeft = '22px';
 
-      this.button.style.height = "32px";
-      this.button.style.width = "70%";
-      this.button.style.left = "15%";
+      this.button.style.height = '32px';
+      this.button.style.width = '70%';
+      this.button.style.left = '15%';
     } else {
       this.text.style.fontSize = '28px';
       this.span.style.fontSize = '28px';
       this.span.style.marginLeft = '28px';
 
-      this.button.style.width = "80%";
-      this.button.style.left = "20%";
-      this.button.style.height = "48px";
+      this.button.style.width = '80%';
+      this.button.style.left = '20%';
+      this.button.style.height = '48px';
     }
   }
 }
